d28e6195130ab2d80a3792a98a8fd86a82ddf891
caoshiwei <caoshiwei@lightinthebox.com>
2018-05-24 17:29:18 +0800

    增加日志分析

--- application/controllers/Job.php
+++ application/controllers/Job.php
@@ -11,16 +11,22 @@ class Job extends CI_Controller{
 
     public function run()
     {
+        log_message('debug','Job run is initialized');
         $this->load->logic('jobLogic');
         $jobName= $this->input->get('jobName');
+        if(!$jobName)
+            log_message('debug','未获取到jobName');
+
         $param = json_decode($this->input->post('param'),true);
         $this->jobLogic->setParam($param,true);
-
+        log_message('debug','成功设置参数');
         if(method_exists($this->jobLogic,$jobName))
         {
+            log_message('debug','任务存在开始执行');
             $resLogId = $this->jobLogic->_taskBegin();
             $resJob =$this->jobLogic->$jobName();
             $this->jobLogic->_taskEnd($resLogId,$resJob);
+            log_message('debug','任务运行完毕');
         }
     }
 }
