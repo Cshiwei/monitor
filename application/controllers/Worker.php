c53921cd7a5c960dec4caf75b9a8cf0b30734a2d
caoshiwei <caoshiwei@lightinthebox.com>
2018-06-04 16:15:45 +0800

    test

--- application/controllers/Worker.php
+++ application/controllers/Worker.php
@@ -19,7 +19,7 @@ class Worker extends CI_Controller{
         $http = new swoole_http_server("0.0.0.0", 9505);
         $http->set (array(
             'worker_num' => 8,		//worker进程数量
-            'daemonize' => True,	//守护进程设置成true
+            'daemonize' => False,	//守护进程设置成true
             'max_request' => 10000,	//最大请求次数，当请求大于它时，将会自动重启该worker
             'dispatch_mode' => 1,
         ));
