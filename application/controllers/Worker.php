c66c3df5bea46995f0d631e63afdddae48d025f7
caoshiwei <caoshiwei@lightinthebox.com>
2018-06-04 18:49:34 +0800

    test

--- application/controllers/Worker.php
+++ application/controllers/Worker.php
@@ -10,12 +10,10 @@ class Worker extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
-
     }
 
-    public function run()
+    public function httpRun()
     {
-        define('CISWOOLE', TRUE);
         $http = new swoole_http_server("0.0.0.0", 9505);
         $http->set (array(
             'worker_num' => 8,		//worker进程数量
@@ -27,6 +25,59 @@ class Worker extends CI_Controller{
         $http->on('request', array($this, 'onRequest'));
         $http->on('start', array($this, 'onStart'));
         $http->start();
+    }
+
+    public function TaskRun()
+    {
+        $task = new swoole_server('0.0.0.0', 9505);   // 允许所有IP访问
+        $task->set(array(
+            'worker_num' => 4,   // 一般设置为服务器CPU数的1-4倍
+            'task_worker_num' => 1,  // task进程的数量（一般任务都是同步阻塞的，可以设置为单进程单线程）
+            'daemonize' => true,  // 以守护进程执行
+            'package_eof' => PHP_EOL,  // 设置EOF
+            'open_eof_split' => true,  // 按照EOF进行分包，防止TCP粘包
+            //  'task_ipc_mode' => 1,  // 使用unix socket通信，默认模式
+            //  'log_file' => '/data/log/queue.log' ,    // swoole日志
+
+            // 数据包分发策略（dispatch_mode=1/3时，底层会屏蔽onConnect/onClose事件，
+            // 原因是这2种模式下无法保证onConnect/onClose/onReceive的顺序，非请求响应式的服务器程序，请不要使用模式1或3）
+            //  'dispatch_mode' => 2,        // 固定模式，根据连接的文件描述符分配worker。这样可以保证同一个连接发来的数据只会被同一个worker处理
+        ));
+
+        $task->on('WorkerStart', array($this, 'onWorkerStart'));
+        $task->on('Receive',array($this,'onReceive'));
+        $task->on('Task', array($this,'onTask'));
+        $task->on('Finish', array($this,'onFinish'));
+        $task->start();
+    }
+
+    public function onReceive($serv, $fd, $from_id, $data )
+    {
+        $str  = "=========== onReceive ============ \n";
+        $str .= "Get Message From Client $fd:$data \n";
+        echo $str;
+        $serv->task( $data );
+    }
+
+    public function onTask($serv, $task_id, $src_worker_id, $data)
+    {
+        $data   = trim($data);
+        $param  = json_decode( $data , true );
+        $str    = "=========== onTask ============ \n";
+        $str   .= var_export($param, 1);
+        echo $str;
+        log_message('debug','接到任务请求');
+        $jobId = $param['jobId'];
+        $resJob = $this->runJob($jobId);
+        return $resJob;
+    }
+
+    public function onFinish($serv, $task_id, $data)
+    {
+        $str  = "=========== onFinish ============ \n";
+        $str .= "Task $task_id finish ! \n";
+        $str .= var_export($data, 1);
+        echo $str;
     }
 
     /**
@@ -62,6 +113,12 @@ class Worker extends CI_Controller{
     {
         log_message('debug','接收到任务请求');
         $jobId = $request->get['jobId'];
+        $resJob = $this->runJob($jobId);
+        $response->end(json_encode($resJob));
+    }
+
+    public function runJob($jobId)
+    {
         log_message('debug',"获取到任务ID{$jobId}");
         $this->load->logic('jobLogic');
         $resJob = $this->jobLogic->run($jobId);
@@ -69,6 +126,6 @@ class Worker extends CI_Controller{
         if($resJob['errNo']!=0)
             log_message('error',$resJob['errMsg']);
 
-        $response->end(json_encode($resJob));
+        return $resJob;
     }
 }
